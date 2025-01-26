1639131d07a042782235adb25ea8688b459f8833
KiRist <37296174+KiRist-code@users.noreply.github.com>
2025-01-26 21:00:54 +0900

    [FEAT]Add Artist Page & some featured artists

--- src/routes/artists.tsx
+++ src/routes/artists.tsx
@@ -5,6 +5,14 @@ import { GlobalStyle } from "../shared/global-style";
 import { Artist } from "../model/artist.model";
 import { getArtistList } from "../service/artists.service";
 import { styled } from "styled-components";
+import {
+  Member,
+  Member_li,
+  Member_ul,
+  Name_warp,
+  Profile,
+} from "../components/about/member";
+import featuredArtists from "../data/FeaturedArtists.json";
 
 const Container_warp = styled.div`
   margin: 40px auto;
@@ -31,10 +39,29 @@ const TextDiv = styled.div`
     font-weight: 600; /*semibold*/
   }
 
+  &.SubTitle {
+    font-size: 30px;
+    font-weight: 500;
+  }
+
   &.Description {
     padding-top: 1%;
     font-size: 15px;
   }
+`;
+
+const Artists_warp = styled.div`
+  margin: 10px;
+  margin-top: 20px;
+  display: flex;
+  justify-content: left;
+  flex-direction: column;
+`;
+
+const Artists_subHead = styled.div`
+  display: flex;
+  justify-content: left;
+  align-items: center;
 `;
 
 const Artists = () => {
@@ -56,6 +83,72 @@ const Artists = () => {
         <ArtistHeader>
           <TextDiv className="Title">Artists</TextDiv>
         </ArtistHeader>
+        <Artists_warp>
+          <Artists_subHead>
+            <TextDiv className="SubTitle">Featured Artist</TextDiv>
+            <TextDiv className="Description">
+              Rooftop's Featured Artists
+            </TextDiv>
+          </Artists_subHead>
+          <Member_ul>
+            {Object.keys(featuredArtists).map((value: string) => (
+              <Member_li>
+                <a
+                  href={
+                    featuredArtists[value as keyof typeof featuredArtists].link
+                  }
+                  rel="noreferrer"
+                  target="_blank"
+                >
+                  <Profile
+                    id="profile"
+                    style={{
+                      backgroundImage: `url(/images/profiles/${
+                        featuredArtists[value as keyof typeof featuredArtists]
+                          .profile
+                      })`,
+                    }}
+                  />
+                </a>
+                <Member>
+                  <Name_warp>
+                    <div
+                      id="name"
+                      style={{
+                        fontWeight: `500`,
+                      }}
+                    >
+                      {value}
+                    </div>
+                    <div
+                      id="role"
+                      style={{
+                        fontSize: `13px`,
+                      }}
+                    >
+                      {
+                        featuredArtists[value as keyof typeof featuredArtists]
+                          .role
+                      }
+                    </div>
+                  </Name_warp>
+                  <div
+                    id="introduce"
+                    style={{
+                      fontSize: `13px`,
+                      lineHeight: `18px`,
+                    }}
+                  >
+                    {
+                      featuredArtists[value as keyof typeof featuredArtists]
+                        .introduce
+                    }
+                  </div>
+                </Member>
+              </Member_li>
+            ))}
+          </Member_ul>
+        </Artists_warp>
       </Container_warp>
       <Footer></Footer>
     </>
