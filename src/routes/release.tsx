894e9f7ed5f637e09c462cd4d4702af4ba93a405
KiRist <37296174+KiRist-code@users.noreply.github.com>
2025-01-26 17:25:35 +0900

    [FIX]Fix never read error on release.tsx

--- src/routes/release.tsx
+++ src/routes/release.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+// import { useEffect } from "react";
 import Footer from "../components/footer";
 import Header from "../components/header";
 import { GlobalStyle } from "../shared/global-style";
